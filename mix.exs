cfe57789827d40fd17f773d7cacaa963420bc59f
RobertDober <robert.dober@gmail.com>
2021-08-26 10:33:13 +0200

    coveralls updated

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule Extractly.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:excoveralls, "~> 0.10.3", only: :test},
+      {:excoveralls, "~> 0.14.2", only: :test},
       {:dialyxir, "~> 1.0.0-rc", only: [:dev, :test], runtime: false},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
