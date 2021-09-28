35f75159557e54580b23c7775fb82e169a7b7c3a
RobertDober <robert.dober@gmail.com>
2021-09-28 09:22:16 +0200

    Fixes #22

--- test/extractly/toc/render_test.exs
+++ test/extractly/toc/render_test.exs
@@ -1,14 +1,23 @@
-defmodule Test.Extractly.Toc.RenderTest do
+  defmodule Test.Extractly.Toc.RenderTest do
   use ExUnit.Case
   doctest Extractly.Toc, import: true
 
-  import Extractly.Toc, only: [render: 1]
+  import Extractly.Toc, only: [render: 1, render: 2]
 
   @easy_peasy ["# One Headline"]
   describe "default options, renders Markdown" do
     test "easy_peasy" do
       expected = ["- One Headline"]
       assert render(@easy_peasy)  == expected
+    end
+    test "with gh links" do
+      expected = ["- [One Headline](#one-headline)"]
+      assert render(@easy_peasy, gh_links: true)  == expected
+    end
+    test "more stress on gh links" do
+      input = ["# &function/2"]
+      expected = ["- [&function/2](#function2)"]
+      assert render(input, gh_links: true) == expected
     end
   end
 end
